056876bff46c18db15128d6c68c61e44bf6ef648
danilov.and <danilov032.andrey@gmail.com>
2021-06-11 15:33:26 +0300

    Финальная версия

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,4 @@
-import time
-
 import pytest
-
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
@@ -11,10 +8,9 @@ from .pages.basket_page import BasketPage
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = MainPage(browser,
-                        link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-        page.open()  # открываем страницу
-        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
 
     def test_guest_should_see_login_link(self, browser):
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,7 @@
 import time
 import pytest
-
-from pages.basket_page import BasketPage
-from pages.registration_page import RegistrationPage
+from .pages.basket_page import BasketPage
+from .pages.registration_page import RegistrationPage
 from .pages.product_page import ProductPage
 
 
@@ -13,8 +12,20 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_sum_basket()
+    page.should_be_name_product_to_basket()
+
+
 @pytest.mark.parametrize('link_', ["offer0", "offer1", "offer2", "offer3", "offer4", "offer5", "offer6",
                                    pytest.param("offer7", marks=pytest.mark.xfail), "offer8", "offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link_):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={link_}"
     page = ProductPage(browser, link)
@@ -49,12 +60,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -63,27 +77,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_in_the_basket_of_product()
     basket_page.should_be_text_empty_basket()
-
-# @pytest.mark.login
-# class TestLoginFromProductPage():
-#     @pytest.fixture(scope="function", autouse=True)
-#     def setup(self):
-#         self.product = ProductFactory(title="Best book created by robot")
-#         # создаем по апи
-#         self.link = self.product.link
-#         yield
-#         # после этого ключевого слова начинается teardown
-#         # выполнится после каждого теста в классе
-#         # удаляем те данные, которые мы создали
-#         self.product.delete()
-#
-#     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-#         page = ProductPage(browser, self.link)
-#         # дальше обычная реализация теста
-#
-#     def test_guest_should_see_login_link(self, browser):
-#         page = ProductPage(browser, self.link)
-#         # дальше обычная реализация теста
 
 
 class TestUserAddToBasketFromProductPage():
@@ -94,7 +87,6 @@ class TestUserAddToBasketFromProductPage():
         page = RegistrationPage(browser, link)
         page.open()
         page.reg_account()
-
 
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
